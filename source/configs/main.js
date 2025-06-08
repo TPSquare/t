a8a4392f30cfbd0e60d3eb51509c37769f51e206
ThanhPhu <132542965+TPSquare@users.noreply.github.com>
2025-06-08 18:36:57 +0700

    Update main.js

--- source/configs/main.js
+++ source/configs/main.js
@@ -1,4 +1,4 @@
-export const port = "8002";
+export const port = "10000";
 export const localStorageKey = "adltgnolstcpetnpe";
 export const languages = ["en", "vi"];
 export const defaultLanguage = "en";
